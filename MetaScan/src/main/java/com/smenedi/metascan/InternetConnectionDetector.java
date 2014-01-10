b8ed37a81c87cef3295160f964e4f31d345dfc34
Satish Menedi <satishmenedi@ufl.edu>
2014-01-10 11:04:42 -0500

    formatted

--- MetaScan/src/main/java/com/smenedi/metascan/InternetConnectionDetector.java
+++ MetaScan/src/main/java/com/smenedi/metascan/InternetConnectionDetector.java
@@ -3,31 +3,30 @@ package com.smenedi.metascan;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
- 
+
 public class InternetConnectionDetector {
- 
+
     private Context _context;
- 
-    public InternetConnectionDetector(Context context){
+
+    public InternetConnectionDetector(Context context) {
         this._context = context;
     }
- 
+
     /**
      * Checking for all possible internet providers
-     * **/
-    public boolean isConnectingToInternet(){
+     * *
+     */
+    public boolean isConnectingToInternet() {
         ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
-          if (connectivity != null)
-          {
-              NetworkInfo[] info = connectivity.getAllNetworkInfo();
-              if (info != null)
-                  for (int i = 0; i < info.length; i++)
-                      if (info[i].getState() == NetworkInfo.State.CONNECTED)
-                      {
-                          return true;
-                      }
- 
-          }
-          return false;
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+
+        }
+        return false;
     }
 }
